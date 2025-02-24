7452319c6f36c1c73f0fc001db9bcb6a129448b7
Mint <meltedmint.liquid.purify@gmail.com>
2025-02-24 22:51:40 +0900

    reset

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AdminController;
-
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'home']);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::get('/home', [AdminController::class, 'index']);
